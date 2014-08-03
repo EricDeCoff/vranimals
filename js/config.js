45f438c7ebd5c47f1bcc5be55e6a762bfd880295
Eric DeCoff <ericdecoff@gmail.com>
2014-08-03 15:31:01 -0400

    vrah - Initial Commit

---
+++ js/config.js
@@ -0,0 +1,1 @@
+require.config({optimize:"uglify2",baseUrl:"js",paths:{domReady:"libs/domReady",jquery:"libs/jquery-2.1.1.min",underscore:"libs/underscore.min",fastclick:"libs/fastclick",bootstrap:"libs/bootstrap.min"},shim:{underscore:{deps:[],"export":"_"},jquery:{deps:[],"export":"$"},bootstrap:{deps:["jquery"],"export":"$.fn.popover"}}}),require(["./js/app.js"]);
